f13c45d5f5fbc57df1df76494dc0ec7e7f205293
Alex Strive <alexstrive@pm.me>
2020-10-01 16:36:20 +0300

    add interaction

--- src/ClassCounter.tsx
+++ src/ClassCounter.tsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react'
 class ClassCounter extends Component {
   state = { value: 0 }
 
+  handleClick = () => {
+    this.setState({ value: this.state.value + 1 })
+  }
+
   render() {
     return (
       <div>
         <h2>Class Counter</h2>
-        {this.state.value}
+        <button onClick={this.handleClick}>Click me! {this.state.value}</button>
       </div>
     )
   }
--- src/FunctionalCounter.tsx
+++ src/FunctionalCounter.tsx
@@ -3,10 +3,14 @@ import React, { useState, FC } from 'react'
 const FunctionalCounter: FC = () => {
   const [value, setValue] = useState(0)
 
+  const handleClick = () => {
+    setValue(value + 1)
+  }
+
   return (
     <div>
       <h2>Functional Counter</h2>
-      {value}
+      <button onClick={handleClick}>Click me! {value}</button>
     </div>
   )
 }
